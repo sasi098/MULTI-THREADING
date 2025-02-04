0d3cf45bd2bca37330c90ccaf64dcd8ca06d9d12
Sasankredd <126907638+Sasankredd@users.noreply.github.com>
2025-02-04 23:24:49 +0530

    NAME,PRIORITY,ID

--- src/Multitextends.java
+++ src/Multitextends.java
@@ -2,7 +2,7 @@ public class Multitextends extends Thread{
 
     public void run(){//THI IS LIKE A CRITICAL STATE
         for(int i=0;i<10;i++){
-            System.out.println(i);
+            System.out.println(Thread.currentThread().getName() + " : " +  i + " and the priority is : " + Thread.currentThread().getPriority());
             try{
                 Thread.sleep(100);//IMPLEMENTING SLEEP THREAD
             }
@@ -18,10 +18,13 @@ public class Multitextends extends Thread{
         //AFTER THIS THERE ARE TWO PARALLEL THREADS. ONE IS MAIN THREAD AND ANOTHER IS RUN THREAD OR M1 THREAD
         //ONCE HECK THE OUPTU AFTER RUNNING TWO TO THREE TIMES
         m1.join(); //MI COMPLETES ITS TASK AFTER WARD IT WILL ALOWW M2,M3 TO WORK PARALLELLY
+        m1.setPriority(10);//10 is the max priority ; 1 is the less priority
         Multitextends m2 = new Multitextends();
+        m2.setName("Thread - 2");
         m2.start();
 
         Multitextends m3 = new Multitextends();
+        m3.setName("Thread - 3");
         m3.start();
     }
 }

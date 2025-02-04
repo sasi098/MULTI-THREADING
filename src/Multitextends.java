881910380270137e45e5c8dca4686d287c6949bc
Sasankredd <126907638+Sasankredd@users.noreply.github.com>
2025-02-04 23:16:18 +0530

    JOIN

--- src/Multitextends.java
+++ src/Multitextends.java
@@ -2,7 +2,7 @@ public class Multitextends extends Thread{
 
     public void run(){//THI IS LIKE A CRITICAL STATE
         for(int i=0;i<10;i++){
-            System.out.println("hey this i user thread");
+            System.out.println(i);
             try{
                 Thread.sleep(100);//IMPLEMENTING SLEEP THREAD
             }
@@ -12,13 +12,16 @@ public class Multitextends extends Thread{
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Multitextends m1 = new Multitextends();//this class name  ->> ENTER INTO A NEW STATE
         m1.start(); // ENTER INTO RUN STATE -> THE IT WILL GOES INTO CRITICAL SECTION OR RUNNING STATE WHICH IS EXECUTED BY RUN METHOD ABOVE
         //AFTER THIS THERE ARE TWO PARALLEL THREADS. ONE IS MAIN THREAD AND ANOTHER IS RUN THREAD OR M1 THREAD
         //ONCE HECK THE OUPTU AFTER RUNNING TWO TO THREE TIMES
-        for(int i=0;i<10;i++){
-            System.out.println("hey this i main thread");
-        }
+        m1.join(); //MI COMPLETES ITS TASK AFTER WARD IT WILL ALOWW M2,M3 TO WORK PARALLELLY
+        Multitextends m2 = new Multitextends();
+        m2.start();
+
+        Multitextends m3 = new Multitextends();
+        m3.start();
     }
 }

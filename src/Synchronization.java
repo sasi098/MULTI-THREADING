ca67edf716fca5363ee9be50e803a8d22dafc82a
Sasankredd <126907638+Sasankredd@users.noreply.github.com>
2025-02-05 21:07:43 +0530

    SYNCHRONIZATION

---
+++ src/Synchronization.java
@@ -0,0 +1,48 @@
+import java.beans.Expression;
+
+class Multiplier{
+    public void multiply(int k){
+        for(int i=1;i<=10;i++){
+            System.out.println(k + " * " + i + " : " + (k*i));
+            try{
+                Thread.sleep(100);
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
+
+
+class Multiplier1 extends Thread{
+    Multiplier m1;
+    public Multiplier1(Multiplier m1){
+        this.m1 = m1;
+    }
+    int k = 0;
+    public void number(int k){
+        this.k = k;
+    }
+    public void run(){
+        m1.multiply(k);
+    }
+}
+
+
+public class Synchronization {
+    public static void main(String[] args) {
+        Multiplier m = new Multiplier();
+        Multiplier1 m1 = new Multiplier1(m);
+        m1.number(10);
+        m1.start();
+
+        Multiplier1 m2 = new Multiplier1(m);
+        m2.number(15);
+        m2.start();
+
+        Multiplier1 m3 = new Multiplier1(m);
+        m3.number(20);
+        m3.start();
+    }
+}
